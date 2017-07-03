8a39d77ddcf280e02668bae8f3f54e1cd757aabe
Geoff Garside <geoff@geoffgarside.co.uk>
2017-07-03 06:52:39 +0100

    Add batch-job service

---
+++ batch-job/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	log.Println("Starting batch job, reticulating splines...")
+	time.Sleep(2 * time.Minute)
+	log.Println("Finished batch job.")
+}
